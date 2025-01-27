34173ab8143fb7b8d4908a4e78da1f4a8aca45c3
visilii <workregor@mail.ru>
2025-01-27 18:53:15 +0300

    chapter 3 done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
 use specs::prelude::*;
-use std::cmp::{max, min};
 use specs_derive::Component;
-use rand;
+use std::cmp::{min, max};
 
 #[derive(Component)]
 struct Position {
@@ -17,9 +16,6 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct RandomMover {}
-
 #[derive(Component, Debug)]
 struct Player {}
 
@@ -32,6 +28,10 @@ impl GameState for State {
         ctx.cls();
 
         self.run_systems();
+        player_input(self, ctx);
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -44,25 +44,104 @@ impl GameState for State {
 
 impl State {
     fn run_systems(&mut self) {
-        let mut rw = RandomWalker{};
-        rw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
 
-struct RandomWalker {}
+#[derive(PartialEq, Clone, Copy)]
+enum TileType {
+    Wall,
+    Floor
+}
 
-impl<'a> System<'a> for RandomWalker {
-    type SystemData = (ReadStorage<'a, RandomMover>, WriteStorage<'a, Position>);
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
 
-    fn run(&mut self, (shall_move, mut pos): Self::SystemData) {
-        for (_shall_move, pos) in (&shall_move, &mut pos).join() {
-            let dx = rand::random::<i32>() % 3 - 1;
-            let dy = rand::random::<i32>() % 3 - 1;
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
 
-            pos.x = (pos.x + dx).rem_euclid(80);
-            pos.y = (pos.y + dy).rem_euclid(50);
+    for x in 0..80 {
+        map[xy_idx(x, 0)]  = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
 
+    for y in 0..50 {
+        map[xy_idx(0, y)]  = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _ in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.3, 0.3, 0.3), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.8, 0.8, 0.8), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        let dest = xy_idx(pos.x + dx, pos.y + dy);
+        if map[dest] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + dx));
+            pos.y = min(49, max(0, pos.y + dy));
+        }
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 
+                => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 
+                => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 
+                => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 
+                => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad7 
+                => try_move_player(-1, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad9 
+                => try_move_player(1, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad1 
+                => try_move_player(-1, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad3 
+                => try_move_player(1, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -79,8 +158,9 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<RandomMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
@@ -92,19 +172,6 @@ fn main() -> rltk::BError {
         })
         .with(Player {})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 4, y: i * 2 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('g'),
-                fg: RGB::named(rltk::GREEN),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(RandomMover{})
-            .build();
-    }
 
     rltk::main_loop(context, gs)
 
